e4c38d1161006a945f70c786ee367575968abc8b
Xander Calzado <calzadoxander@gmail.com>
2026-02-25 21:28:08 +0800

    Update Task11.php

--- Task11.php
+++ Task11.php
@@ -1,40 +1,47 @@
 <?php
 
-class Book{
-    public $title;
-    public $author;
-    public $year;
-    public $isAvailable = true;
+class Book {
+    private $title;
+    private $author;
+    private $year;
+    private $isAvailable;
 
-    public function __construct($title, $author, $year){
-        $this->title =$title;
-        $this->author =$author;
-        $this->year =$year;
-    }
-    public function borrowBook(){
-        $this->isAvailable = false;
-        echo $this->title . " has been borrowed. <br>";
+    public function __construct($title, $author, $year) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->year = $year;
+        $this->isAvailable = true; 
     }
 
-    public function returnBook(){
+    public function borrow() {
+        if ($this->isAvailable) {
+            $this->isAvailable = false;
+            echo "You've successfully borrowed: {$this->title} <br>";
+        } else {
+            echo "Sorry, {$this->title} is already borrowed. <br>";
+        }
+    }
+
+    public function returnBook() {
         $this->isAvailable = true;
-        echo $this->title . " has been returned. <br>";
+        echo "{$this->title} has been returned to the library. <br>";
     }
 
-    public function displayInfo(){
-        echo "Title: " . $this->title . "<br>";
-        echo "Author: " . $this->author . "<br>";
-        echo "Year: " . $this->year . "<br>";
-        echo "Availability: " . ($this->isAvailable ? "Available ✅" : "Not Available ❌") . "<br> <br>";
+    public function display() {
+        echo "Book Details: <br>";
+        echo "Title: {$this->title} <br>";
+        echo "Author: {$this->author} <br>";
+        echo "Published Year: {$this->year} <br>";
+        echo "Status: " . ($this->isAvailable ? 'Available ✅' : 'Not Available ❌') . "<br><br>";
     }
-
 }
-$book1 = new Book ("Diary of Wimpy Kid ", "Jeff Kinney", 2004);
-    $book1->displayInfo();
+$book1 = new Book("Diary of a Wimpy Kid", "Jeff Kinney", 2004);
+$book1->display();
 
-    $book1->borrowBook();
-    $book1->displayInfo();
+$book1->borrow();
+$book1->display();
 
-    $book1->returnBook();
-    $book1->displayInfo();
+$book1->returnBook();
+$book1->display();
+
 ?>
